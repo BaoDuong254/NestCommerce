d192e2fd3c870b99cc74722ab311ec6eb9809682
GiaBao <duonggiabao254@gmail.com>
2025-11-06 11:22:39 +0700

    chore: remove authentication and post management modules and related files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,11 @@
 import { ClassSerializerInterceptor, Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { PostsModule } from "src/routes/posts/posts.module";
 import { SharedModule } from "./shared/shared.module";
-import { AuthModule } from "src/routes/auth/auth.module";
 import { APP_INTERCEPTOR } from "@nestjs/core";
 
 @Module({
-  imports: [PostsModule, SharedModule, AuthModule],
+  imports: [SharedModule],
   controllers: [AppController],
   providers: [
     AppService,
